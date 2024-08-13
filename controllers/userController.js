10fd5c79b1e6dc6142fa6084948504a4dd7f03c3
Amos Godwin <amosgodwin500@gmail.com>
2024-08-13 15:03:17 +0100

    fixed the import bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,13 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
-  // Registration logic
+    const user = new User(req.body);
+    try {
+      const newUser = await user.save();
+      res.status(200).json(newUser);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
 };
 
 exports.login = async (req, res) => {
