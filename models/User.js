2c002448eccd5a7439fa1d1c23d94c1dbfdcb908
Amos Godwin <amosgodwin500@gmail.com>
2024-08-14 02:50:17 +0100

    updated user schema

--- models/User.js
+++ models/User.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
   profile_picture: { type: String },
   followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+  resetPasswordToken: { type: String },
+  resetPasswordExpiry: { type: Date }
 });
 
 module.exports = mongoose.model('User', UserSchema);
