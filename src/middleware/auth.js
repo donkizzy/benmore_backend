b906d646d904a84e4072f11d6de1c6760e608785
Amos Godwin <amosgodwin500@gmail.com>
2024-08-15 08:18:24 +0100

    fixed bearer token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.header('Authorization');
+  const token = req.header('Authorization').replace('Bearer ', '');
   if (!token) return res.status(401).send({message: 'Access Denied'});
 
   try {
